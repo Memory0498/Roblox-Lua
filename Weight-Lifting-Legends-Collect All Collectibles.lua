687ee0dbe03364d3bf1cc66478876c1d9bdc611a
Memory <64565404+Memory0498@users.noreply.github.com>
2021-09-18 21:02:46 -0400

    Made this

---
+++ Weight-Lifting-Legends-Collect All Collectibles.lua
@@ -0,0 +1,6 @@
+for i, v in pairs(game.Workspace:GetDescendants()) do
+    if v.Parent.Parent.Name == "Collectibles" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles2" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles3" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles4" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles5" and v:IsA("Part") then
+        wait(0.09)
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+    end
+end
