91f10277480a831358cd1019dd68fbd14767b740
Memory <64565404+Memory0498@users.noreply.github.com>
2021-09-18 21:07:30 -0400

    Correctly commented game link

--- Weight-Lifting-Legends-Collect-All-Collectibles.lua
+++ Weight-Lifting-Legends-Collect-All-Collectibles.lua
@@ -1,6 +1,6 @@
---[
-Game link: https://www.roblox.com/games/7101662992/Weight-Lifting-Legends
-]--
+--[[
+https://www.roblox.com/games/7101662992/Weight-Lifting-Legends
+--]]
 
 for i, v in pairs(game.Workspace:GetDescendants()) do
     if v.Parent.Parent.Name == "Collectibles" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles2" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles3" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles4" and v:IsA("Part") or v.Parent.Parent.Name == "Collectibles5" and v:IsA("Part") then
